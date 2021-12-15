affab4e9d460b1f48e15ef2b6c4ffa7d7f6ed3bb
slavaOlshanechky <ekaterina.olshanetskaya@gmail.com>
2021-12-15 13:32:22 +0200

    add countersList.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,10 +2,6 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0); //useState can contain [],{},[{}]
-  const [tags, setTags] = useState(
-    // []
-    ["tag1", "tag2", "tag3"]
-  );
 
   const formatCount = () => {
     return count === 0 ? "Empty" : count;
@@ -23,30 +19,6 @@ const Counter = () => {
   const handleDecrement = () => {
     setCount((prevState) => prevState - 1);
   };
-  const handleTagChange = (id) => {
-    // setTags(["tag4", "tag5"]);
-    setTags((prevState) => prevState.filter((tag) => tag !== id));
-    // console.log(id);
-  };
-
-  const renderTags = () => {
-    return (
-      tags.length !== 0 &&
-      tags.map((tag) => (
-        <li
-          key={tag}
-          className="btn btn-primary btn-sm m-2"
-          onClick={() => handleTagChange(tag)}
-        >
-          {tag}
-        </li>
-      ))
-    );
-  };
-
-  if (tags.length !== 0) {
-    return <ul>{renderTags()}</ul>;
-  }
 
   return (
     <>
---
+++ src/components/countersList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CountersList = () => {
+  return (
+    <>
+      <h1>CountersList</h1>
+    </>
+  );
+};
+export default CountersList;
